ec09f6425793e44220563a10858734bb9d7f1bc4
seham <316160038@sha.edu.eg>
2017-09-30 20:24:40 +0200

    2nd commit

--- app/src/main/java/com/example/pc/web/MainActivity.java
+++ app/src/main/java/com/example/pc/web/MainActivity.java
@@ -2,6 +2,17 @@ package com.example.pc.web;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView) findViewById(R.id.textview);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fe81cc86b1b198376a650a28075a13d4";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: "+ response.substring(0,500));
+
+//                        JsonObject jsonObject=new JsonObject(response);
+//                        JsonArray jsonArray=jsonObject.getAsJsonArray("results");
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/example/pc/web/Models/MovieModel.java
@@ -0,0 +1,17 @@
+package com.example.pc.web.Models;
+
+/**
+ * Created by pc on 30/09/2017.
+ */
+
+public class MovieModel {
+
+    int vote_count,id;
+    Boolean video;
+    double vote_average,popularity;
+    String title,poster_path,original_language,overview,release_date;
+
+
+
+
+}
